9097da38b660a1ed1df3c257839b93052a0bd13d
Inamul <inamulhoque097@gmail.com>
2022-05-27 17:28:14 +0530

    Modify: Modified with using Switch Case

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -1,22 +1,24 @@
 public class EmpWageComputation {
-    public static void main(String args[]) {
-        System.out.println("welcome to Employee wage computation program\n");
-        int isFullTime = 1;
-        int isPartTime = 2;
-        int EmpRatePerHour = 20;
+    public static void main(String[] args) {
+//        int isFullTime = 1;
+//        int isPartTime = 2;
+        int EmpRatePerHr = 20;
         int emphrs = 0;
         int empwage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == isFullTime) {
-            System.out.println("Employee is present full time");
-            emphrs = 8;
-        } else if (empCheck == isPartTime) {
-            System.out.println("Employee is present part time");
-            emphrs = 4;
-        }else {
-            System.out.println("Employee is absent");
+        int empCheck = (int) (Math.floor(Math.random()*10)%3);
+        switch (empCheck){
+            case 1:
+                System.out.println("Employee is present full time");
+                emphrs = 8;
+                break;
+            case 2:
+                System.out.println("Employee is present part time");
+                emphrs = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
         }
-        empwage = emphrs * EmpRatePerHour;
-        System.out.println(empwage);
+        empwage = emphrs * EmpRatePerHr;
+        System.out.print("Today's wage is: "+empwage);
     }
 }
