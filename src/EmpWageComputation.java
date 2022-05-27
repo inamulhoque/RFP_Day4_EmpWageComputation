b81658265aa4851acd2d2e3945174d09c1620fe1
Inamul <inamulhoque097@gmail.com>
2022-05-27 17:42:48 +0530

    Added: Maximum working hour is set i.e. 100 hours a month

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -4,12 +4,13 @@ public class EmpWageComputation {
         int isPartTime = 2;
         int EmpRatePerHour = 20;
         int MaxWorkingDay = 20;
+        int MaxWorkingHr = 100;
         int empTotalWage = 0;
         int emphrs = 0;
         int day = 1;
         int totalWorkingHour = 0;
 
-        while(day <= MaxWorkingDay) {
+        while(day <= MaxWorkingDay && totalWorkingHour <= MaxWorkingHr) {
             day++;
             double empCheck = Math.floor(Math.random() * 10) % 3;
             switch ((int) empCheck){
@@ -25,9 +26,11 @@ public class EmpWageComputation {
                     System.out.println("Employee is absent");
             }
             totalWorkingHour=totalWorkingHour+emphrs;
+            totalWorkingHour++;
         }
 
         empTotalWage = totalWorkingHour * EmpRatePerHour;
         System.out.println("Total salary of a month: "+empTotalWage);
+        System.out.println("Total Working Hour of a month: "+totalWorkingHour);
     }
 }
