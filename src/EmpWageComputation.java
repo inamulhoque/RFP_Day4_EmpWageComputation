cb6955e93e08bbdff5c33b783ac116caf1298c31
Inamul <inamulhoque097@gmail.com>
2022-05-27 17:26:05 +0530

    Modify: Part time employee added

--- src/EmpWageComputation.java
+++ src/EmpWageComputation.java
@@ -1,20 +1,22 @@
 public class EmpWageComputation {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         System.out.println("welcome to Employee wage computation program\n");
-        int isFulltime = 1;
+        int isFullTime = 1;
+        int isPartTime = 2;
         int EmpRatePerHour = 20;
         int emphrs = 0;
         int empwage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == isFulltime) {
-            System.out.println("Employee is present");
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == isFullTime) {
+            System.out.println("Employee is present full time");
             emphrs = 8;
-        } else {
+        } else if (empCheck == isPartTime) {
+            System.out.println("Employee is present part time");
+            emphrs = 4;
+        }else {
             System.out.println("Employee is absent");
-            emphrs = 0;
         }
-        System.out.print("Today's wage is: ");
         empwage = emphrs * EmpRatePerHour;
-        System.out.print("Rs."+empwage);
+        System.out.println(empwage);
     }
 }
